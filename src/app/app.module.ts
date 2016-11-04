ffb26b5b774994822c5c0ab68a3a928b656de1ba
kelvin knigton <keltheceo@gmail.com>
2016-11-04 16:45:37 -0700

    added user-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component'
-import { UserItemComponent } from './user-item/user-item.component'
+import { UserListComponent } from './user-list/user-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    UserItemComponent 
+    UserListComponent
   ],
   imports: [
     BrowserModule,
